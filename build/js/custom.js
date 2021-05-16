213919e5d8349be9912d5bf95b73f818d30ffac7
Eko Bayu <ekobayu@mail.com>
2021-05-16 15:15:57 +0700

    remove unused css and js, add latest library

--- build/js/custom.js
+++ build/js/custom.js
@@ -1,63 +1,3 @@
-$(".incr-btn").on("click", function (e) {
-    var $button = $(this);
-    var oldValue = $button.parent().find('.quantity').val();
-    $button.parent().find('.incr-btn[data-action="decrease"]').removeClass('inactive');
-    if ($button.data('action') == "increase") {
-        var newVal = parseFloat(oldValue) + 1;
-    } else {
-        // Don't allow decrementing below 1
-        if (oldValue > 1) {
-            var newVal = parseFloat(oldValue) - 1;
-        } else {
-            newVal = 1;
-            $button.addClass('inactive');
-        }
-    }
-    $button.parent().find('.quantity').val(newVal);
-    e.preventDefault();
-})
-
-$('.carouselSlider').owlCarousel({
-    items: 1,
-    margin: 0,
-    dots: false,
-    autoHeight: false,
-    autoWidth: false,
-    responsiveClass: true,
-    loop: true,
-    autoplay: 100,
-    autoplayHoverPause: true,
-    navText: "<>",
-})
-
-$('.carouselProduct').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    navText: "<>",
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
-    }
-})
-
-$('ul.navbar-nav li.dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-  }, function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-  });
-
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
@@ -72,15 +12,21 @@ $('#return-to-top').click(function() {      // When arrow is clicked
     }, 500);
 });
 
-/* wait for images to load */
-$(window).load(function() {
-    $('.sp-wrap').smoothproducts();
+var hashTagActive = "";
+$(".scroll").on("click touchstart", function (event) {
+  if (hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+    event.preventDefault();
+    //calculate destination place
+    var dest = 0;
+    if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+      dest = $(document).height() - $(window).height();
+    } else {
+      dest = $(this.hash).offset().top;
+    }
+    //go to destination
+    $('html,body').animate({
+      scrollTop: dest
+    }, 2000, 'swing');
+    hashTagActive = this.hash;
+  }
 });
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
